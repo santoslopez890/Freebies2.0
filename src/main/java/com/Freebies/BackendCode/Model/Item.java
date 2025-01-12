e5dfa3963ec3639659dc56c6e29faa1b97176617
Santos Herrera <santoslopez890@gmail.com>
2025-01-12 16:54:46 -0500

    Worked on Item Entity

--- src/main/java/com/Freebies/BackendCode/Model/Item.java
+++ src/main/java/com/Freebies/BackendCode/Model/Item.java
@@ -1,6 +1,165 @@
 package com.Freebies.BackendCode.Model;
 
-public class Item {
-    int change=1;
-    int something=2;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+/**
+ * A Item.
+ */
+@Entity
+@Table(name = "item")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@SuppressWarnings("common-java:DuplicatedBlocks")
+public class Item implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private Double price;
+
+    @Column(name = "location")
+    private String location;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "image")
+    private String image;
+
+
+
+
+
+    // jhipster-needle-entity-add-field - JHipster will add fields here
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public Item id(Long id) {
+        this.setId(id);
+        return this;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Item name(String name) {
+        this.setName(name);
+        return this;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return this.price;
+    }
+
+    public Item price(Double price) {
+        this.setPrice(price);
+        return this;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getLocation() {
+        return this.location;
+    }
+
+    public Item location(String location) {
+        this.setLocation(location);
+        return this;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public Item description(String description) {
+        this.setDescription(description);
+        return this;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return this.image;
+    }
+
+    public Item image(String image) {
+        this.setImage(image);
+        return this;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+
+
+
+
+
+
+    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Item)) {
+            return false;
+        }
+        return id != null && id.equals(((Item) o).id);
+    }
+
+    @Override
+    public int hashCode() {
+        // see https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
+        return getClass().hashCode();
+    }
+
+    // prettier-ignore
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + getId() +
+                ", name='" + getName() + "'" +
+                ", price=" + getPrice() +
+                ", location='" + getLocation() + "'" +
+                ", description='" + getDescription() + "'" +
+                ", image='" + getImage() + "'" +
+                "}";
+    }
 }
