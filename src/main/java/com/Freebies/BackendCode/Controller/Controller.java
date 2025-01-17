3f0b78ca23bae6e4e6159db1ca171a17790f72ea
Santos Herrera <santoslopez890@gmail.com>
2025-01-16 23:29:42 -0500

    Added scraper I need to configure it to spring

--- src/main/java/com/Freebies/BackendCode/Controller/Controller.java
+++ src/main/java/com/Freebies/BackendCode/Controller/Controller.java
@@ -1,7 +1,9 @@
 package com.Freebies.BackendCode.Controller;
 
 import com.Freebies.BackendCode.Model.Item;
+import com.Freebies.BackendCode.Model.RawItem;
 import com.Freebies.BackendCode.Service.ItemService;
+import com.Freebies.BackendCode.Service.Scraper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,8 +21,15 @@ public class Controller {
         itemService.saveItem(item);
         return "new item added";
     }
+    @PostMapping
     @GetMapping("/getItems")
     public List<Item> getItems(){
         return itemService.getAllItems();
     }
+    @PostMapping
+    @GetMapping("/getScrapedItems")
+    public List<RawItem> getScrapedItems() throws InterruptedException {
+        return Scraper.scrapeData("home","19805");
+    }
+
 }
